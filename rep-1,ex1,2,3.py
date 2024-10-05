9d8da296918a00b2756e8fe3d311c7be8fd0db5d
Holchenkov <holcenkovaleksej@gmail.com>
2024-10-05 17:11:56 +0300

    Create rep-1,ex1,2,3.py

---
+++ rep-1,ex1,2,3.py
@@ -0,0 +1,32 @@
+#Задание 1
+integer_var = 10
+float_var = 9.6
+string_var = "Hello"
+list_var = [1,2,3,4,5]
+tuple_var = ("red",9)
+dict_var = {"Name": "Vanya", "Age": 20}
+set_var = {1,2,3,4,5}
+bool_var = True
+print(integer_var) 
+print(float_var)
+print(string_var)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(set_var) 
+print(bool_var)
+#Задание 2
+name = "Alexei"
+age = 17
+average_grade = 4.7
+favorite_movies = ["Star Wars", "Harry Potter", "Survivor"]
+personal_info = ("surname : Holchenkov","school number : 33","avg grade : 4.7")
+skills = {"Speed": 100, "strength": 52}
+visited_countries = {"China","Germany","Spain"}
+has_driver_license = False
+#Задание 3 
+CPU = "Intel core i5-10400f"
+volume_of_RAM_in_gigabytes = 16
+internet_speed_in_mbps = 91.58 
+Options = [CPU, volume_of_RAM_in_gigabytes, internet_speed_in_mbps]
+print(Options)
