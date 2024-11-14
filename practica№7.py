a827d960b494755bd9e90db3bfaf9f564db2855c
Holchenkov <holcenkovaleksej@gmail.com>
2024-11-14 10:39:43 +0300

    Update practica№7.py

--- practica№7.py
+++ practica№7.py
@@ -32,3 +32,34 @@ for i in x:
         if a[-3:] == ".js":
             v.append('new_' + a)
 print(v)
+#Задание 2
+word_numb = ["ноль", "один", "два", "три", "четыре", "пять",
+"шесть", "семь", "восемь", "девять"]
+n = int(input("Введите число от 0 до 9:" ))
+if n > 9:
+    print('Введите число <= 9')
+else:
+    for i in range(n+1):
+        print(word_numb[i])
+#Задание 3
+bin_sy = ['11011111', '11011101', '11000111', '11011100',
+'11011110']
+gg = []
+for i in bin_sy:
+    print(int(i, 2))
+    gg.append(int(i, 2))
+print(f"Максимальное  значение - {max(gg)}")
+print(f"Минимальное значение - {min(gg)}")
+#Задание 4
+matrix = [
+    [-446, 281, -80],
+    [465, 432, -122],
+    [13, 'error', 8]
+]
+summ = []
+for g in matrix:
+    for i, mat in enumerate(g):
+        if isinstance(mat, str):
+            g[i] = len(mat)
+            summ += g[i]
+print(summ)
